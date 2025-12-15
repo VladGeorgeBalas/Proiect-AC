0c0c47f5eb7c0fe6eaa655e3fe88e977b6e02d3a
Your Name <vlad_george.balas@stud.acs.upb.ro>
2025-12-15 15:38:04 +0200

    inceput lucru la memorie

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod simulation;
+
 use image::ImageReader;
 
 fn main() {
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,4 +1,18 @@
+use std::ptr::null;
 use rhdl::prelude::*;
+use rhdl::bits;
+
+#[derive(Digital, PartialEq, Eq)]
+pub struct ImageMatrix<const WIDTH: usize, const HEIGHT: usize>{
+    pub data : [[Bits<U8>; WIDTH]; HEIGHT]
+}
+
+impl<const WIDTH: usize, const HEIGHT: usize> ImageMatrix<WIDTH, HEIGHT>
+{
+    pub fn create(data: Vec<Vec<u8>>) -> Self{
+        
+    }
+}
 
 pub fn simulate() -> Result<(), RHDLError>
 {
